6167c79728115eb477fd30524117c71ba8e6731d
MiguelGMata <miguelgmata.dev@gmail.com>
2024-09-14 23:17:38 +0200

    creation projet reactjs

---
+++ src/components/pages/home/HomeScreen.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './homeScreen.css';
+
+
+const HomeScreen = () =>{
+    return(
+        <main>
+            <h1>Home</h1>
+        </main>
+    )
+}
+
+export default HomeScreen
