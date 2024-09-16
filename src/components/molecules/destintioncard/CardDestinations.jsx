e95598e736e71788d76c6b6a19f12985a97e583e
MiguelGMata <miguelgmata.dev@gmail.com>
2024-09-16 21:44:13 +0200

    creation HotelsCard avec data

--- src/components/molecules/destintioncard/CardDestinations.jsx
+++ src/components/molecules/destintioncard/CardDestinations.jsx
@@ -3,10 +3,10 @@ import './cardDestinations.css';
 
 const destinations = [
   { id: 1, title: 'Majorque', imageUrl: 'https://ik.imagekit.io/logoMGM/Derni%C3%A8reMinute/mallorca-1.png?updatedAt=1726395884335' },
-  { id: 2, title: 'Santorini', imageUrl: 'https://ik.imagekit.io/logoMGM/Derni%C3%A8reMinute/santorini-1.jpg?updatedAt=1726395884168' },
+  { id: 2, title: 'Santorin', imageUrl: 'https://ik.imagekit.io/logoMGM/Derni%C3%A8reMinute/santorini-1.jpg?updatedAt=1726395884168' },
   { id: 3, title: 'Ibiza', imageUrl: 'https://ik.imagekit.io/logoMGM/Derni%C3%A8reMinute/ibiza-1.jpg?updatedAt=1726395884215' },
-  { id: 4, title: 'Grande Canarie', imageUrl: 'https://ik.imagekit.io/logoMGM/Derni%C3%A8reMinute/gran-canaria-1.jpg?updatedAt=1726395884235' },
-  { id: 5, title: 'Puerto Rico', imageUrl: 'https://ik.imagekit.io/logoMGM/Derni%C3%A8reMinute/puerto-rico-1.jpg?updatedAt=1726395884231' },
+  { id: 4, title: 'Îles Canaries', imageUrl: 'https://ik.imagekit.io/logoMGM/Derni%C3%A8reMinute/gran-canaria-1.jpg?updatedAt=1726395884235' },
+  { id: 5, title: 'Porto Rico', imageUrl: 'https://ik.imagekit.io/logoMGM/Derni%C3%A8reMinute/puerto-rico-1.jpg?updatedAt=1726395884231' },
   { id: 6, title: 'Corse', imageUrl: 'https://ik.imagekit.io/logoMGM/Derni%C3%A8reMinute/corse-1.jpg?updatedAt=1726420022963' }
 ];
 
